e67cbc6f8736eb4f196ac7880b65c6306104903e
unknown <innovativeroh@gmail.com>
2024-09-03 02:46:34 +0530

    Register Page

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { toast, ToastContainer } from "react-toastify";
 import { motion } from "framer-motion";
-import register from "@/../public/register.png";
+import register from "@/../public/reg.jpg";
 import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import logo from "@/../public/logo.png";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
 
@@ -69,18 +72,161 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmit }) => {
   return (
     <>
       <div className="rounded-lg text-gray-900 flex justify-center">
-        <div className="max-w-screen-xl rounded-lg m-0 sm:m-10 bg-white sm:rounded-lg flex justify-center flex-1">
-          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+        <div className="max-w-screen-xl gap-10 rounded-lg m-0 sm:m-10 bg-white sm:rounded-lg flex justify-center flex-1">
+          <div className="lg:w-1/3 xl:w-5/12 p-6 sm:p-12">
             <div className="flex flex-col items-center">
-              <h1 className="text-3xl xl:text-3xl font-extrabold">Register To Get Started</h1>
+              <h1 className="text-3xl xl:text-3xl font-extrabold">
+                Register To Get Started
+              </h1>
               <p>Secure Access to Your Personal Dashboard</p>
+
+              <Tabs defaultValue="email" className="w-[400px] mt-8">
+                <TabsList className="w-full">
+                  <TabsTrigger value="email" className="w-full">
+                    Email
+                  </TabsTrigger>
+                  <TabsTrigger value="mobile" className="w-full">
+                    Mobile
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent value="email">
+                  <div className="flex flex-col items-center">
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="fname" className="font-semibold">
+                        First Name
+                      </Label>
+                      <Input
+                        type="fname"
+                        id="#"
+                        placeholder="Enter Your First Name"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="lname" className="font-semibold">
+                        Last Name
+                      </Label>
+                      <Input
+                        type="lname"
+                        id="#"
+                        placeholder="Enter Your Last Name"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="email" className="font-semibold">
+                        Email
+                      </Label>
+                      <Input
+                        type="email"
+                        id="#"
+                        placeholder="someone@something.com"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="password" className="font-semibold">
+                        Password
+                      </Label>
+                      <Input
+                        type="password"
+                        id="#"
+                        placeholder="Choose a Strong Password"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <button
+                        type="button"
+                        className="transition p-2.5 rounded-2xl bg-[#0AA579] hover:bg-black text-white focus:bg-black font-semibold"
+                        disabled={!method}
+                      >
+                        Sign Up
+                      </button>
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <button
+                        type="button"
+                        className="p-2.5 rounded-2xl bg-black text-white focus:bg-black font-semibold"
+                        disabled={!method}
+                      >
+                        Already have account?
+                      </button>
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <p className="text-sm text-center">I agree to abide by EarnCharge <Link href="#" className="font-semibold"> Terms Conditions</Link> & <Link href="#" className="font-semibold">Privacy Policy</Link></p>
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="mobile">
+                <div className="flex flex-col items-center">
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="fname" className="font-semibold">
+                        First Name
+                      </Label>
+                      <Input
+                        type="fname"
+                        id="#"
+                        placeholder="Enter Your First Name"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="lname" className="font-semibold">
+                        Last Name
+                      </Label>
+                      <Input
+                        type="lname"
+                        id="#"
+                        placeholder="Enter Your Last Name"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="email" className="font-semibold">
+                        Phone
+                      </Label>
+                      <Input
+                        type="text"
+                        id="#"
+                        placeholder="8888866666"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <Label htmlFor="password" className="font-semibold">
+                        Password
+                      </Label>
+                      <Input
+                        type="password"
+                        id="#"
+                        placeholder="Choose a Strong Password"
+                      />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <button
+                        type="button"
+                        className="transition p-2.5 rounded-2xl bg-[#0AA579] hover:bg-black text-white focus:bg-black font-semibold"
+                        disabled={!method}
+                      >
+                        Sign Up
+                      </button>
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <button
+                        type="button"
+                        className="p-2.5 rounded-2xl bg-black text-white focus:bg-black font-semibold"
+                        disabled={!method}
+                      >
+                        Already have account?
+                      </button>
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+                      <p className="text-sm text-center">I agree to abide by EarnCharge <Link href="#" className="font-semibold"> Terms Conditions</Link> & <Link href="#" className="font-semibold">Privacy Policy</Link></p>
+                    </div>
+                  </div>
+                </TabsContent>
+              </Tabs>
             </div>
           </div>
-            <div className="flex-1 text-center hidden lg:flex items-center justify-center">
-              <div className="">
-                <Image src={register} alt="login" />
-              </div>
+          <div className="flex-1 text-center hidden lg:flex items-center justify-center">
+            <div className="p-10">
+              <Image src={register} alt="login" className="rounded-3xl" />
             </div>
+          </div>
         </div>
       </div>
     </>
